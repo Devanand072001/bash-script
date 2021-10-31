3303c22318e5479058c0fce870710cd4bae82cff
devanand072001 <devanand.cs19@bitsathy.ac.in>
2021-10-31 15:00:02 +0530

    functions

--- functions/functions.sh
+++ functions/functions.sh
@@ -17,3 +17,21 @@ function greet(){
     echo "Good morning"
 }
 greet
+
+add(){
+    read -p "Enter value 1: " VAR1
+    read -p "Enter value 2: " VAR2
+    echo "value 1:  $VAR1"
+    echo "value 2: $VAR2"
+    return $((VAR1 + VAR2))
+}
+add
+echo "sum: $?"
+
+var1=10
+var2=20
+multiply(){
+    return $((var1 * var2))
+}
+multiply
+echo "$var1 * $var2 = $?"
